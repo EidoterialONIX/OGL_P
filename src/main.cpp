c4448a7e9bc8f7bbd63fbea04a49ba15d03bedc4
Eidoterial <Eidoterial.p@gmail.com>
2024-09-02 12:28:47 +0300

    Add rect geometry, micro update TRANSFORM_POINTS

--- src/main.cpp
+++ src/main.cpp
@@ -18,7 +18,6 @@ Options _options;
 
 
 
-
 template <class T>
 class Vec_3 {
 public:
@@ -131,9 +130,9 @@ public:
 
     void TRANSFORM_POSITION(Ordinary_Geometry &o_g) const {
         for (int i{ 0 }; i < 3; i++) {
-            o_g.get_Points()._point[i * 3] += _position._x;
-            o_g.get_Points()._point[1 + i * 3] += _position._y;
-            o_g.get_Points()._point[2 + i * 3] += _position._z;
+            o_g.get_Points()._point[i * 3] = _position._x;
+            o_g.get_Points()._point[1 + i * 3] = _position._y;
+            o_g.get_Points()._point[2 + i * 3] = _position._z;
         }
         
     }
@@ -152,9 +151,9 @@ public:
 
         for (int i{ 0 }; i < 3; i++) {
 
-            new_points._point[i * 3] = (o_g.get_Points()._point[i * 3] / (options.WINDOW_SIZE[0] / 2 / 100) - 100) / 100;
+            new_points._point[i * 3] = (o_g.get_Points()._point[i * 3] / (options.WINDOW_SIZE[0] / 2.0f / 100.0f) - 100) / 100.0f;
             
-            new_points._point[1 + i * 3] = (o_g.get_Points()._point[1 + i * 3] / (options.WINDOW_SIZE[1] / 2 / 100) - 100) / 100;
+            new_points._point[1 + i * 3] = -(o_g.get_Points()._point[1 + i * 3] / (options.WINDOW_SIZE[1] / 2.0f / 100.0f) - 100) / 100.0f;
         }
 
         o_g.set_Points_Transform(new_points);
@@ -181,13 +180,15 @@ public:
     void set_Trilangl() {
         TRANSFORM_POSITION(_figur);
 
-        _figur.get_Points()._point[1] += _radius;
+        _figur.get_Points()._point[1] -= _radius;
 
-        _figur.get_Points()._point[3] += _radius / 90 * 45;
-        _figur.get_Points()._point[4] -= _radius / 90 * 45;
+        _figur.get_Points()._point[3] -= (_radius / 90) * 45;
+        _figur.get_Points()._point[4] += (_radius / 90) * 45;
 
-        _figur.get_Points()._point[6] -= _radius / 90 * 45;
-        _figur.get_Points()._point[7] -= _radius / 90 * 45;
+        _figur.get_Points()._point[6] += (_radius / 90) * 45;
+        _figur.get_Points()._point[7] += (_radius / 90) * 45;
+
+        std::cout << (_radius / 90) * 45 << std::endl;
 
         TRANSFORM_SCALE(_figur);
 
@@ -197,6 +198,50 @@ public:
 
 
     Ordinary_Geometry& get_Ordinary_Geometry() { return _figur; }
+
+};
+
+
+
+class Rect : public Transform {
+private:
+
+    Vec_3<float> _size = Vec_3<float>(0.0f, 0.0f, 0.0f);
+
+    Ordinary_Geometry _figur[2];
+
+public:
+
+    Rect() = default;
+
+    Vec_3<float> get_Size() const { return _size; }
+    void set_Size(Vec_3<float> new_size) { _size = new_size; }
+
+    void set_Rect() {
+        TRANSFORM_POSITION(_figur[0]);
+        TRANSFORM_POSITION(_figur[1]);
+
+        /// First figur
+        _figur[0].get_Points()._point[3] += _size._x;
+        _figur[0].get_Points()._point[4] += _size._y;
+
+        _figur[0].get_Points()._point[7] += _size._y;
+
+        /// Second figur
+        _figur[1].get_Points()._point[3] += _size._x;
+
+        _figur[1].get_Points()._point[6] += _size._x;
+        _figur[1].get_Points()._point[7] += _size._y;
+
+        TRANSFORM_SCALE(_figur[0]);
+        TRANSFORM_SCALE(_figur[1]);
+
+        TRANSFORM_POINTS(_figur[0], _options);
+        TRANSFORM_POINTS(_figur[1], _options);
+
+    }
+
+    Ordinary_Geometry& get_Ordinary_Geometry(int index) { return _figur[index]; }
 
 };
 
@@ -217,6 +262,27 @@ public:
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
         glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    }
+
+    void DRAW_RECT(Rect& RECT) {
+        for (int i{ 0 }; i < 2; i++) {
+
+            GLuint points_vbo = 0;
+            glGenBuffers(1, &points_vbo);
+
+            glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(RECT.get_Ordinary_Geometry(i).get_Points_Transform()._point), RECT.get_Ordinary_Geometry(i).get_Points_Transform()._point, GL_STATIC_DRAW);
+
+            glEnableVertexAttribArray(0);
+            glBindBuffer(GL_ARRAY_BUFFER, points_vbo);
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+            glDrawArrays(GL_TRIANGLES, 0, 3);
+
+        }
+
+        
 
     }
 
@@ -264,7 +330,7 @@ void glfwKeyCallback(GLFWwindow* pWindow, int key, int scancode, int action, int
 Draw_on_screen Drawinger;
 
 Trilangl tril;
-
+Rect rect;
 
 int main(void)
 {
@@ -312,15 +378,20 @@ int main(void)
 
     glClearColor(1, 1, 0, 1);
 
-    tril.set_Radius(50.0f);
-    tril.set_Position(Vec_3<float>(600.0f, 200.0f, 0.0f));
+    tril.set_Radius(100.0f);
+    tril.set_Position(Vec_3<float>(0.0f, 0.0f, 0.0f));
 
     tril.set_Trilangl();
 
     tril.get_Ordinary_Geometry().show_Info_Points();
 
 
-    
+    rect.set_Position(Vec_3<float>(0.0f, 0.0f, 0.0f));
+    rect.set_Size(Vec_3<float>(100.0f, 100.0f, 0.0f));
+
+    rect.set_Rect();
+
+
    
 
     /* Loop until the user closes the window */
@@ -330,7 +401,7 @@ int main(void)
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        Drawinger.DRAW_TRILANGL(tril);
+        Drawinger.DRAW_RECT(rect);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(pWindow);
